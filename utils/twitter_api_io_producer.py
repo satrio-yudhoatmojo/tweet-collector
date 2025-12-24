653a6ebd5ecf90e0b47cd62415b7bf31600ab9a8
chromatix <satrio.yudhoatmojo@gmail.com>
2025-12-24 14:36:35 +0700

    update argument in producer

--- utils/twitter_api_io_producer.py
+++ utils/twitter_api_io_producer.py
@@ -10,6 +10,6 @@ class TwitterApiIOProducer:
 
     def enqueue_search_job(self, query=None, query_type=None, next_cursor=None, start_time=None, output_filepath="output/search_results.ndjson"):
         with faktory.connection() as client:
-            params = [query, query_type, next_cursor]
+            params = [query, query_type, next_cursor, output_filepath]
             logging.info(f"Enqueuing TwitterApiIO search job with start_time: {start_time}")
             client.queue("advanced_search", args=(params), at=start_time)
