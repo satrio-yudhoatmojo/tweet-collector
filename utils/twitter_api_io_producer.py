04d0e2e22e7e72db90d534daf614e0c690978307
chromatix <satrio.yudhoatmojo@gmail.com>
2025-12-24 14:42:25 +0700

    update handling if start time none

--- utils/twitter_api_io_producer.py
+++ utils/twitter_api_io_producer.py
@@ -9,6 +9,9 @@ class TwitterApiIOProducer:
         pass
 
     def enqueue_search_job(self, query=None, query_type=None, next_cursor=None, start_time=None, output_filepath="output/search_results.ndjson"):
+        if not start_time:
+            start_time = datetime.datetime.now().isoformat()
+            
         with faktory.connection() as client:
             params = [query, query_type, next_cursor, output_filepath]
             logging.info(f"Enqueuing TwitterApiIO search job with start_time: {start_time}")
